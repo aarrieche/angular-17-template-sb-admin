744c269b4495d3ecde03ed07e4ebc89782bc1a05
aarrieche <arriechealexandre@gmail.com>
2024-01-30 05:33:32 -0300

    :sparkles: feat: Project created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import jQuery from 'jquery';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit{
   title = 'front';
+
+  ngAfterViewInit(): void {
+    if (typeof window !== 'undefined') {
+      (function($) {
+        "use strict";
+    
+        // Add active state to sidebar nav links
+        var path = window.location.href.toString(); // because the 'href' property of the DOM element is the absolute path
+        $("#layoutSidenav_nav .sb-sidenav a.nav-link").each(function() {
+          $(this).addClass("active");
+        });
+    
+        // Toggle the side navigation
+        $("#sidebarToggle").on("click", function(e) {
+          e.preventDefault();
+          $("body").toggleClass("sb-sidenav-toggled");
+        });
+      })(jQuery);
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,16 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { TemplateModule } from './template/template.module'
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    TemplateModule
   ],
   providers: [
     provideClientHydration()
